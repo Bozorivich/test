fd81df6c58b17796c74ff563f3bd475e23640e37
Bozorivich <joshqinbekbozorov08@gmail.com>
2023-09-12 15:46:11 +0500

    add new file

---
+++ index.js
@@ -0,0 +1,275 @@
+// let counrtry = [
+//    {
+//       music: "",
+//    },
+//    {
+//       music: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Australia_Day.jpg/220px-Australia_Day.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://www.ghasrangasht.com/wp-content/uploads/2020/06/Tirana-Albania.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://www.spot.uz/media/img/2018/04/atZpqO15232744148533_l.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://prod.wordpress.usemultiplier.cloud/wp-content/uploads/2022/10/Angola-TW-Hero-Image-1024x768.webp",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://upload.wikimedia.org/wikipedia/commons/7/79/Encamp%28Andorra%29.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://planetofhotels.com/guide/sites/default/files/styles/paragraph__live_banner__lb_image__1880bp/public/live_banner/Antigua-and-Barbuda.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://wikiwandv2-19431.kxcdn.com/_next/image?url=https://upload.wikimedia.org/wikipedia/commons/4/4f/MDQ_skyline.jpg&w=640&q=50",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://i1.wp.com/zoomex.ru/sites/default/files/users/user1/strani/arm.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://traveltimes.ru/wp-content/uploads/2021/10/c6a526c1e801ebea090919ed60b5befd-1024x768.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://a.warbletoncouncil.org/science/densidad-de-poblacin-concepto-cmo-se-calcula-y-ejemplos-3.webp",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://national-travel.ru/wp-content/uploads/wtt-images/2021/08/barbados%20(5).jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://storage.kun.uz/source/6/61Wz1y4-NovZbgnxY1ZZhFqMa6Ga-hAF.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://uz.instudy.uz/wp-content/uploads/2020/10/%D0%91%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D1%8C.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://www.pcclean.io/wp-content/uploads/2021/11/786365456710664792.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://api.human.uz/media/main_images/%D0%B3%D1%80%D1%83%D0%B7%D0%B8%D1%8F.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://aniq.uz/photos/news/kcmIrOBsix2XZBt.jpeg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://i1.wp.com/ticketforplane.ru/sites/default/files/Avtor/images/karti-emiratov.jpg",
+//    },
+//    {
+
+//    },
+//    {
+//       music: "https://grantlar.uz/wp-content/uploads/2019/05/bulgaria-1200x720.jpg",
+//    },
+//    {
+
+//    },
+// ]
+// let promp = prompt('mamlakat nomini kiriting')
+
+
+// // for (let i = 0; i < counrtry.length; i++) {
+
+
+// //       document.write(`
+// //            <div class="wrapper">
+// //            <div class="country">
+// //             <img src="${counrtry[i].music}"/>
+
+
+// //             </div>
+// //             <div class="items">
+// //               </div>
+
+// //            </div>
+// //       `)
+// //    }
+// // }
+
+
+// let audio = document.querySelector('#audio')
+// let play = document.querySelector('.play')
+
+
+
+
+// if(promp === "cent"){
+
+
+
+// }
+
+
+// play.addEventListener('click', () => {
+//    audio.setAttribute('src', './music/50 Cent - Candy Shop.mp3')
+//    audio.play()
+// })
+
+const music = [
+   {
+      title: "sarvar",
+      music: "/music1.mp3"
+   },
+   {
+      title: "jo'shqinbek",
+      music: "/music2.mp3"
+   },
+   {
+      title: "baxtiyor",
+      music: "/music3.mp3"
+   },
+   {
+      title: "gerber",
+      music: "/music4.mp3"
+   },
+   {
+      title: "Ozodbek Nazarbekov",
+      music: "/music5.mp3"
+   },
+   {
+      title: "Yulduz Usmonova",
+      music: "/music6.mp3"
+   },
+   {
+      title: "Rashid Holiqov",
+      music: "/music7.mp3"
+   },
+   {
+      title: "SHoxruxxon",
+      music: "/music8.mp3"
+   },
+   {
+      title: "Munisa Rizayeva",
+      music: "/music9.mp3"
+   },
+   {
+      title: "Toyir Zokirov",
+      music: "/music10.mp3"
+   },
+   {
+      title: "Luiza Rasulova",
+      music: "/music11.mp3"
+   },
+   {
+      title: "SHerali Jo'rayev",
+      music: "/music15.mp3"
+   },
+   {
+      title: "Zohirshox Jo'rayev",
+      music: "/music16.mp3"
+   },
+   {
+      title: "lola",
+      music: "/music17.mp3"
+   },
+   {
+      title: "Ozoda Nursayidova",
+      music: "/music18.mp3"
+   },
+   {
+      title: "Ulug'bek Raxmatullayev",
+      music: "/music19.mp3",
+      img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Faniq.uz%2Fuz%2Fyangiliklar%2Fulugbek-rahmatullaev-internetdagi-eng-zur-suratni-elon-qildi-foto&psig=AOvVaw2q3bIkoT_I7OEw6XV1x1kC&ust=1694092641298000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKjFsIOJloEDFQAAAAAdAAAAABAE"
+   },
+   {
+      title: "oybek",
+      music: "/music20.mp3",
+      img:""
+   },
+   
+
+
+
+
+
+
+
+
+]
+
+const musics = prompt('musicani kiriting')
+
+
+let send = document.querySelector('.play')
+let pause = document.querySelector('.pause')
+let input = document.querySelector('input')
+let audio = document.querySelector('audio')
+
+
+function musicquery() {
+   for (let i = 0; i < music.length; i++) {
+      const element = music[i];
+      console.log(element)
+      if (musics === music[i].title) {
+         send.addEventListener('click', () => {
+            audio.setAttribute('src', `music/${music[i].music}`)
+            audio.play()
+         })
+         
+         
+         
+      }
+      pause.addEventListener('click', () => {
+         if (audio.played) {
+            audio.pause()
+         }
+      })
+   }
+}
+musicquery()
+
+
+
+
